30723ea1d1f72f253b7e7910f563ee322e062430
John Lau <nohjlau@gmail.com>
2020-01-28 02:52:16 -0800

    Implemented isMonotonic and housekeeping

--- LeetCode/0986. Interval List Intersections/0986. Interval List Intersections.js
+++ LeetCode/0986. Interval List Intersections/0986. Interval List Intersections.js
@@ -1,1 +1,36 @@
-// 20.01.26
+/*
+isMonotonic
+20.01.26
+
+T(n) = 0(n)
+*/
+
+var isMonotonic = function(A) {
+  let monotonic = false;
+  if(monotoneDec(A) || monotoneInc(A)) {
+    monotonic = true;
+  }
+  
+  return monotonic;
+};
+
+monotoneDec = function(A) {
+  let monotone = true;
+  for(let i = 0; i < A.length-1; i++) {
+    if(A[i] < A[i+1])
+      monotone = false; 
+  }
+  return monotone;
+}
+
+monotoneInc = function(A) {
+  let monotone = true;
+  for(let i = 0; i < A.length-1; i++) {
+    if(A[i] > A[i+1])
+      monotone = false;
+  }
+  return monotone;
+}
+
+let A = [1, 2, 2, 3];
+console.log(isMonotonic(A));
---
+++ LeetCode/README.md
@@ -0,0 +1,6 @@
+* 20.01.28
+  * 689 [Maximum Sum of 3 Non-Overlapping Subarrays](https://leetcode.com/problems/maximum-sum-of-3-non-overlapping-subarrays/)
+  * 721 [Accounts Merge](https://leetcode.com/problems/accounts-merge/)
+  * 767 [Reorganize String](https://leetcode.com/problems/reorganize-string/)
+  * 785 [Is Graph Bipartite?](https://leetcode.com/problems/is-graph-bipartite/)
+  * 896 [Monotonic Array](https://leetcode.com/problems/monotonic-array/)
